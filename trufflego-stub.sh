c78e8757f914d59e074a7a27495d85b1155fc2d5
bschuedzig <benjamin.schuedzig@mantro.net>
2022-01-15 17:41:43 +0100

    Adds missing variable

--- trufflego-stub.sh
+++ trufflego-stub.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 # Find path of the script (and follow symlink if needed)
+SRC="${BASH_SOURCE[0]}"
 REPO=$(cd -- "$(dirname -- "$([ -L "$SRC" ] && readlink -f "$SRC" || echo "$SRC")")" &>/dev/null && pwd)
 
 set -euo pipefail
